6aea6c52f6ff91fb4c01b338997acd52ad04f3ce
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-11-13 12:50:07 +0100

    first commit

---
+++ BankingApp/Models/CheckingAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BankingApp.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName="varchar")]
+        [Display(Name="Account #")]
+        [RegularExpression(@"\d{6,10}",ErrorMessage ="Account # must between 6 and 10 digits.")]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+        public string Name
+        {
+
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public string ApplicationUserId { get; set; }
+    }
+}
---
+++ BankingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankingApp.Startup))]
+namespace BankingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
